332c4c5aa1cfac841c2925b7ea05b37b71280c1c
iAmScienceMan <63004048+iAmScienceMan@users.noreply.github.com>
2026-05-24 15:02:42 +0300

    не считать покрытие для блока синка команд

--- bot.py
+++ bot.py
@@ -127,7 +127,7 @@ class CoolBot(commands.Bot):
             guild_name = guild.name if guild else f"Unknown Guild ({guild_id})"
             log.info("  %s: %d каналов", guild_name, len(channels))
 
-        try:
+        try:  # pragma: no cover
             self.tree.copy_global_to(guild=GUILD)
             self.tree.clear_commands(guild=None)
             await self.tree.sync()
